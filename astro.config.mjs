0b8d07f04bbed297bf9c021ee4a3cb57b5ab30cb
joonfi <compute.q@yahoo.com>
2026-08-02 21:50:10 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
+// https://astro.build/config
 export default defineConfig({
-  // Astro 5.18+ defaults to 'static', which works for WebUI + API routes
+  site: 'https://ssii.fzoirm.com', // ADD THIS LINE
   adapter: cloudflare({
     platformProxy: { enabled: true },
   }),
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp',
+      config: {
+        // Allow sharp to run at build time only
+        allowBuild: () => true, 
+      }
+    }
+  }
 });   
