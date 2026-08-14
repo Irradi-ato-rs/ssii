9109528bdc09024c18458cec177f5309dbf7efaa
Joon Fi <compute.q@yahoo.com>
2026-08-14 21:20:05 +0800

    Update: env remove extent

--- src/env.d.ts
+++ src/env.d.ts
@@ -9,7 +9,7 @@ interface Env {
 type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 
 declare namespace App {
-  interface Locals extends Runtime {
+  interface Locals {
     user?: {
       email: string;
       role: 'governance' | 'admin' | 'executive' | 'engineer';
